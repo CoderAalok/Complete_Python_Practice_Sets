59ab8615418989c423c23cf0001335a40e310d94
Alok_Yadav <yadavaalok565@gmail.com>
2025-10-21 22:14:15 +0545

    Basic_python

--- Python_Basic - Copy/.py
+++
@@ -1,1 +0,0 @@
-#Random string generating until a given string not generated
---
+++ Python_Basic - Copy/9_Uncommon_strr.py
@@ -0,0 +1,10 @@
+strr1 = ("Large Language Model Used For Web Application").split()
+strr2 = ("Large Diffusion Model")
+
+print("Uncommon words are found from strr1: ")
+
+for i in strr1:
+    if not i in strr2:
+        print(i, end='->')
+    else:
+        continue
