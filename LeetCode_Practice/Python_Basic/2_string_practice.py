1029528838afcfc081276869d9b1d297db7d2ef8
Alok Yadav <yadavaalok565@gmail.com>
2025-10-23 11:10:06 +0545

    Delete LeetCode_Practice/Python_Basic directory

--- LeetCode_Practice/Python_Basic/2_string_practice.py
+++
@@ -1,4 +0,0 @@
-strr = 'pneomonoultramicroscopicsilivolcanococious Volcano supertaious'.split()
-vol = 'aeiou'
-rel = [i for i in strr if all(v in i for v in vol)]
-print(rel)
--- LeetCode_Practice/Python_Basic/tulpe.py
+++
@@ -1,3 +0,0 @@
-tuple = (2,3,53,5,7,3)
-del tuple(6)
-print(tuple)
