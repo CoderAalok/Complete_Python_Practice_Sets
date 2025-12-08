6a6e66d41e35b055a726c378dee74c459f1319c1
Alok_Yadav <yadavaalok565@gmail.com>
2025-12-08 22:23:55 +0545

    Vector

--- Vector.py
+++ Vector.py
@@ -10,16 +10,37 @@ class Vector:
         if len(self.vec1) != len(vec2.vec1):
             raise ValueError("Vector dimension not equal!")
     
-
+    
+    def __format(self,sign):
+        format = []
+        for i,v in enumerate(sign):
+            if v > 0:
+                format.append(f"+ {v}k{i}")
+            elif v < 0:
+                format.append(f"- {abs(v)}k{i}")
+            else:
+                format.append(f"{v}k{i}")
+            
+        if format:
+            
+            format_result = " ".join(format)
+            if format_result.startswith("+ "):
+                return format_result[2:]
+            return format_result
+        
+        return None
+                
+                
     def __add__(self,vec2):
         self.__check(vec2)
         add = [i+j for i,j in zip(self.vec1,vec2.vec1)]
-        return  add
+        return  self.__format(add)
     
     def __sub__(self,vec2):
         self.__check(vec2)
         sub = [i-j for i,j in zip(self.vec1,vec2.vec1)]
-        return  sub
+        return  self.__format(sub)
+
 
     
     def __mul__(self,vec2):
@@ -34,36 +55,18 @@ class Vector:
         
         magnitude = (magnitude_vec1*magnitude_vec2)*np.cos(self.pi)
         return round(magnitude,2)
-
-def format_sign(sign):
-    if not sign :
-        return None
-    
-    result = []
-    for i,v in enumerate(sign):
-    
-        if v > 0:
-            result.append(f"+ {v}k{i}")
-        elif v < 0 :
-            result.append(f"- {abs(v)}k{i}")
-        else:
-            result.append(f"{v}k{i}")
-    
-    if result:
-        s = ' '.join(result)
-        if s.startswith('+ '):
-            return s[2:]
-        return s
     
 
+if __name__ == "__main__":  # Using this to make this file name Vector ->> __main__
+    a = (2,3,-4,3,-1)
+    b = (2,-9,0,1,-5)
 
-a = (2,3,-4,3,-1)
-b = (1,-9,0,1,-5)
-
-v1 = Vector(a)
-v2 = Vector(b)
-print(f"Addition :   {(format_sign(v1+v2))}")
-print(f"Subtration : {(format_sign(v1-v2))}")
-prod,mag = v1*v2
-print(f"DotProduct : {prod}")
-print(f"Magnitude:   {mag}rad")
+    v1 = Vector(a)
+    v2 = Vector(b)
+    
+    print(f"Addition :   {(v1+v2)}")
+    print(f"Subtration : {(v1-v2)}")
+    
+    prod,mag = v1*v2
+    print(f"DotProduct : {prod}")
+    print(f"Magnitude:   {abs(mag)}rad")
