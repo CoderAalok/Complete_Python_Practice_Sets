6ab594ff7037f7eb763eb9f525da56f335f5bb54
CoderAalok <yadavaalok565@gmail.com>
2026-01-06 02:28:06 +0545

    Basic JSON

--- JSON.py
+++ JSON.py
@@ -3,53 +3,39 @@ import json
 # convert JSON into python dictionary(Deserialization)
 # data = '{"files":["music","photos"],"fruits":["banana","apple","coconut"]}'
 # print(data) # by this way can't access value using key
+data = '{"files":["music","photos"],"fruits":["banana","apple","coconut"]}'
+# # print(data) # by this way can't access value using key
 # result = json.loads(data)
 # print(result)
+# print(result, type(result))
+# re = json.dumps(result)
+# print(type(re), re)
 # print(result['files'])
 # print(type(result))
-
-# ======================================================================
-
-# Convert dictionry  into JSON file(Seralization)
-# data = {
-#     "Python":"JSON",
-#     "dict":"object",
-#     "list tuple":"array",
-#     "None":"null",
-#     "file":[10,True,None,90.12,('python','machine learning')]
-# }
-
-# parsed = json.dumps(data)
-# print(parsed)
-
-
-# parsed1 = json.dumps(data, indent=4)
-# print(parsed1)
 
 # ======================================================================
 
 # Read JSON from file
 with open("data.json","r")as f:
     add_data = (json.load(f))
+# with open("data.json","r")as f:
+#     add_data = (json.load(f))
 
 # ======================================================================
     
-# # Write JSON from file
-# data = {
-#     "Python":"JSON",
-#     "dict":"object",
-#     "list tuple":"array",
-#     "None":"null",
-#     "file":[10,True,None,90.12,('python','machine learning')]
-# }
-
-# append on file
+with open("data.json","r")as f:
 # data = {
 #     "Computer":"vision",
 #     "Compitative":"Programming",
 #     "Data Analytics":"Data Sciencetist"
+#     "Data Analytics":"Data Sciencetist",
+#     1: False
 # }
 # add_data.append(data)
 # with open("data.json","w+")as f:
 #     print(json.dump(add_data, f, indent=4))
+# # add_data.append(data)
+# # with open("data.json","w")as f:
+# #     json.dump(add_data, f, indent=4)
 
+# print(int(list(add_data[1].keys())[-1])+123)
