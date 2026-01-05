d7b3344584b9a800d72dedd6d46ed2f390a45458
CoderAalok <yadavaalok565@gmail.com>
2026-01-05 23:46:16 +0545

    pattern

--- 06_pattern.py
+++ 06_pattern.py
@@ -75,3 +75,87 @@ for i in range(n):
         print('',chr(ord('a')+m),end='-') 
     print()
     
+#solid Triangle
+
+# size = 7
+# for r in range(size):
+#     for c in range(r,size):
+#         print(' ',end='')
+#     for c in range(r):
+#         print('*',end='')
+#     for c in range(r+1):
+#         print('*',end='')
+    
+#     print()
+    
+#Solid Triangle
+# size = 7
+# for i in range(size):
+#     for j in range(i,size):
+#         print(' ',end='')
+#     for j in range(i+1):
+#         print('*', end=' ')
+        
+#        #Hollow triangle 
+#         # if i == size-1 or j == i or j==0: 
+#         #     print('*',end=' ')
+#         # else:
+#         #     print(' ',end=' ')
+#     print()
+    
+# size = 7 
+# for i in range(size):
+#     for j in range(i+1):
+#         print(' ',end='')
+#     for j in range(i,size):
+#         print('*', end=' ') #with space
+#     # for j in range(i+1,size):#without space
+#     #     print('*', end='')
+    
+#     print()
+
+n = 5
+for i in range(n):
+    for j in range(n):
+        if j == 0 or  i+j == n-1  or i == 0:
+            print('*' , end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+    
+    for j in range(i,n):
+        print("-",end=' ')
+    
+    for k in range(i):
+        print(chr(ord('a')+k),end='-')
+       
+    for m in range(i+1):
+        print(chr(ord('a')+m),end='-')
+    
+    for j in range(i,n):
+        print("-",end=' ')
+    print()
+print("---Code Camp---")
+for i in range(n):  
+    for a in range(i):
+        print(' ','-',end='')
+    
+    for r in range(i,n):
+        print('',chr(ord('a')+r),end='-')
+    
+    for m in range(i,n-1):
+        print('',chr(ord('a')+m),end='-') 
+    print()
+
+
+
+n = int(input("Enter a length of pattern: ").strip())
+boolean = int(input("Enter a number (0 or 1): ").strip())
+
+if boolean:
+    for i in range(1,n):
+        print("*"*i)
+else:
+    for i in range(n):
+        print("*"*(n-i))
+        
